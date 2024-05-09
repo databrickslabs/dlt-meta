3cd58823b6519757b7c86fd59d41354c5d9fd776
ravi-databricks <37003292+ravi-databricks@users.noreply.github.com>
2024-05-09 16:28:52 -0700

    Removed unused flatten function

--- src/onboard_dataflowspec.py
+++ src/onboard_dataflowspec.py
@@ -388,24 +388,6 @@ class OnboardDataflowspec:
             if not onboarding_row[field]:
                 raise Exception(f"Missing field={field} in onboarding_row")
 
-    def flatten_data(y):
-        out = {}
-
-        def flatten(x, name=''):
-            if type(x) is dict:
-                for a in x:
-                    flatten(x[a], name + a + '_')
-            elif type(x) is list:
-                i = 0
-                for a in x:
-                    flatten(a, name + str(i) + '_')
-                    i += 1
-            else:
-                out[name[:-1]] = x
-
-        flatten(y)
-        return out
-
     def __get_bronze_dataflow_spec_dataframe(self, onboarding_df, env):
         """Get bronze dataflow spec method will convert onboarding dataframe to Bronze Dataflowspec dataframe.
 
