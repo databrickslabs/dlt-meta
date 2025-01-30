e72ac07640875a7ac09a6d5fda38b208172806cb
ravi-databricks <ravi.gawai@databricks.com>
2025-01-30 13:45:11 -0800

    Added spark shuffle parition and disbaled spark ui for unit tests

--- tests/utils.py
+++ tests/utils.py
@@ -26,9 +26,6 @@ class DLTFrameworkTestCase(unittest.TestCase):
         )
         self.spark = configure_spark_with_delta_pip(builder).getOrCreate()
         self.spark.conf.set("spark.sql.shuffle.partitions", "4")
-        self.spark.conf.set("spark.driver.memory", "4g")
-        self.spark.conf.set("spark.executor.memory", "8g")
-        self.spark.conf.set("spark.executor.memoryOverhead", "1024m")
         self.deltaPipelinesMetaStoreOps = DeltaPipelinesMetaStoreOps(self.spark)
         self.deltaPipelinesInternalTableOps = DeltaPipelinesInternalTableOps(self.spark)
         self.sc = self.spark.sparkContext
